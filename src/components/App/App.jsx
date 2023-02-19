2be53f831829ed3d3692ed0ec12470f6cb591f52
Dasha Yarkovska <yarkovska11@gmail.com>
2023-02-19 19:49:49 +0200

    -

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -15,8 +15,6 @@ import {
 } from './reexport';
 
 export function App() {
-  //contacts: JSON.parse(localStorage.getItem('contacts')) ??  [...] instead of doing check in componentDidMount()
-
   const [contacts, setContacts] = useState([
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
